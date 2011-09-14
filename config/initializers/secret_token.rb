fd5090af55f6536fa655dc303eac7e16fdc14b96
vovan <vovan@vovan-rezonans.(none)>
2011-09-14 10:21:14 +0300

    test commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magaz::Application.config.secret_token = 'd23fa64a9e42e38fef95bb7fb81ca1249f793641ba0f4dd7c65ed85a4f8beb12aa324fff26de61aa3ab770940027bf55c2ba888c1c5baa6ac3251feaeff1c4b5'
